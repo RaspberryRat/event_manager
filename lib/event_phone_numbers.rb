45b375578cd2287f71eab6fcc72f5eabd573cf57
Graham <git@grahamirvine.com>
2022-06-01 12:07:49 -0600

    refactored clean_phone_numbers into separate method

--- lib/event_phone_numbers.rb
+++ lib/event_phone_numbers.rb
@@ -1,6 +1,27 @@
 require "csv"
 require "pry-byebug"
 
+
+def clean_phone_numbers(phone_num)
+  to_remove = {
+    "-" => "",
+    " " => "",
+    "(" => "",
+    ")" => "",
+    "." => "",
+  }
+  phone_num = phone_num.gsub(/[- ().]/, to_remove)
+
+  if phone_num.length == 10
+    phone_num.insert(3, "-")
+    phone_num.insert(7, "-")
+  elsif phone_num.length == 11 && phone_num[0] == "1"
+      phone_num = phone_num[1..]
+  else
+    phone_num = ""
+  end
+end
+
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
@@ -10,26 +31,9 @@ contents = CSV.open(
 contents.each do |row|
 
   name  = row[:first_name]
-  phone_num = row[:homephone]
+  phone_num = clean_phone_numbers(row[:homephone])
 
-  phone_num = phone_num.gsub("-", "")
-  phone_num = phone_num.gsub(" ", "")
-  phone_num = phone_num.gsub("(", "")
-  phone_num = phone_num.gsub(")", "")
-  phone_num = phone_num.gsub(".", "")
-
-  unless phone_num.length == 10
-    if phone_num.length == 11 && phone_num[0] == "1"
-      phone_num = phone_num[1..]
-    else
-      phone_num = ""
-    end
-  end
-
-  if phone_num.length == 10
-    phone_num.insert(3, "-")
-    phone_num.insert(7, "-")
-  end
+  
 
 
   puts "#{name}'s number is #{phone_num}"
