a18e355029e014dd5ba3a1af0b07d3b7c95db929
Graham <git@grahamirvine.com>
2022-06-02 09:00:36 -0600

    created methods to determine best days of week to buy ads

--- lib/event_time_registered.rb
+++ lib/event_time_registered.rb
@@ -8,13 +8,29 @@ def hour_registered(time)
   Time.strptime(time, "%m/%d/%y %k").hour
 end
 
+def weekday_registered(time)
+  day = Time.strptime(time, "%m/%d/%y %k").wday
+
+  day = case day
+  when 0 then "Sunday"
+  when 1 then "Monday"
+  when 2 then "Tuesday"
+  when 3 then "Wednesday"
+  when 4 then "Thursday"
+  when 5 then "Friday"
+  when 6 then "Saturday"
+  end
+  day
+end
+  
+
 def time_of_day_registered(time)
   time_slots = {
-    "early_morning" => [1, 2, 3, 4, 5, 6, 7, 8],
-    "morning" => [9, 10, 11, 12],
-    "mid_day" => [13, 14, 15, 16],
-    "evening" => [17, 18, 19, 20],
-    "night" => [21, 22, 23, 0],
+    "Early Morning" => [1, 2, 3, 4, 5, 6, 7, 8],
+    "Morning" => [9, 10, 11, 12],
+    "Mid Day" => [13, 14, 15, 16],
+    "Evening" => [17, 18, 19, 20],
+    "Night" => [21, 22, 23, 0],
   }
 
   hour_count = []
@@ -34,21 +50,47 @@ def best_time_to_buy_ads(time)
   time.map do |key, value|
     hour_count.push(value)
   end
-  print "Best time to buy ads is:\n"
+  print "\nBest time to buy ads is:\n"
   time.map do |key, value|
   puts key if value >= hour_count.sort[-2]
   end
 end
+
+def day_of_week_registered_count(day_array)
+  days = day_array.reduce(Hash.new(0)) do |result, vote|
+    result[vote] += 1
+    result
+  end
+  best_day_to_buy_ads(days)
+end
+
+def best_day_to_buy_ads(day_array)
+  day_count = []
+  day_array.map do |key, value|
+    day_count.push(value)
+  end
+  print "\nBest days to buy ads are:\n"
+  day_array.map do |key, value|
+  puts "#{key}" if value >= day_count.sort[-2]
+  end
+end
+
 
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
-# TODO find a way to now you empty array here
+
 hours_registered = []
+days_registered = []
 contents.each do |row|
   registration_date = row[:regdate]
   hours_registered.push(hour_registered(registration_date))
+
+  days_registered.push(weekday_registered(registration_date))
 end
+
 time_of_day_registered(hours_registered)
+day_of_week_registered_count(days_registered)
+
